89068ac92879a0c13ce03d54e634914d35ad46b8
Esther Akinniyi <okaforestherc@yahoo.com>
2022-09-11 09:31:05 -0700

    task 7

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+table File  22 lines (21 sloc)  297 Bytes
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+if (c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
