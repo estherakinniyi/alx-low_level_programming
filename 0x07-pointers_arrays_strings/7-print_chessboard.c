b3f6a367b7bdb2347412a677cf225f221bfed125
Esther Akinniyi <okaforestherc@yahoo.com>
2022-09-26 10:08:23 -0700

    adding task

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,20 +3,20 @@
 /**
  * print_chessboard - prints the chessboard
  * @a: input pointer.
- * Return: void.
+ * Return: no return.
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, j = 0;
+	unsigned int i, m = 0;
 
 	for (i = 0; i < 64; i++)
 	{
 		if (i % 8 == 0 && i != 0)
 		{
-			j = i;
+			m = i;
 			_putchar('\n');
 		}
-		_putchar(a[i / 8][i - j]);
+		_putchar(a[i / 8][i - m]);
 	}
 	_putchar('\n');
 }
